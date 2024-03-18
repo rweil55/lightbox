5b6165cf9994ab25bec48d1ddba170b7669d849f
rweil55 <github@shaw-weil.com>
2024-03-18 10:56:41 -0400

    2024-03-18

--- make.php
+++ make.php
@@ -2,7 +2,6 @@
 
 <head>
     <title>My First Web Page</title>
-    <script src="fslightbox.js"></script>
 </head>
 
 <body>
@@ -10,79 +9,27 @@
     $msg = "";
     $dire = "king-county-park";
     $msg .= OneLightbox($dire);
-    print $msg;
+    print $msg . date("Y-m-d H:i:s");
 
     function OneLightbox($dire)
     {
-        $file = "<html>
-        <head>
-        <title>$dire</title>
-        <script src='fslightbox.js'></script>
-        <style type='text/css'>
-        .example-initial-animation {
-            animation: initial-animation 2s ease;
-        }
-        </style>
-        </head>
-        <body>
-            <h1>$dire</h1>
-        <script>
-
-var lightbox = new FsLightbox();
-lightbox.props.sources = [";
+        $template =  file_get_contents("makeTemplate.html");
+        $template = str_replace("<title>", "<title>$dire", $template);
+        $images = "\n";
         foreach (new DirectoryIterator($dire) as $fileInfo) {
             if ($fileInfo->isDot()) continue;
             if ($fileInfo->isDir()) continue;
-            $filename = $fileInfo->getFilename();
-            $file .= "'$dire/$filename',";
+            $fileName = $fileInfo->getFilename();
+            $images .= "<a href='$dire/$fileName' data-lg-size='1600-2400'>
+            <img alt='file Name is $fileName' src='$dire/thumbs/thumbs_$fileName' />
+        </a>
+        ";
         }
-        $file = substr($file, 0, -1); // remove last comma
-        $file .= "];\n
-   /// lightbox.props.showThumbsOnMount = true;
-   // lightbox.props.initialAnimation = 'example-initial-animation';
-    lightbox.open(0);
-";
+        $template = str_replace('dynamicEl: [', 'dynamicEl: [' . $images, $template);
 
-        /*  
-
-        $file .= "
-            lightbox.props.onInit = () => {
-                console.log('Lightbox initialized');
-            };
-            lightbox.props.onOpen = () => {
-                console.log('Lightbox opened');
-            };
-            lightbox.props.onClose = () => {
-                console.log('Lightbox closed');
-            };
-            lightbox.props.onShow = (source) => {
-                console.log('Source displayed', source);
-            };
-            lightbox.props.onClose = () => {
-                console.log('Lightbox closed');
-            };
-            lightbox.props.onLoad = () => {
-                console.log('Image loaded');
-            };
-            lightbox.props.onLoadError = (source) => {
-                console.log('Error loading image', source);
-            };
-            lightbox.props.onSlideChange = (current, next) => {
-                console.log('Slide changed', current, next);
-            };
-            lightbox.props.onAction = (action, source) => {
-                console.log('User performed action', action, source);
-                */
-        $file .= "</script>
-      
-        </body>
-        </html>";
-        file_put_contents("$dire.html", $file);
+        file_put_contents("$dire.html", $template);
         return "Created <a href='//127.0.0.1/lightbox/$dire.html' target='make'> $dire.html</a><br>";
     }
-
-
-
     ?>
 
 </body>
---
+++ make2.php
@@ -0,0 +1,55 @@
+<html>
+
+<head>
+    <title>My First Web Page</title>
+</head>
+
+<body>
+    <?php
+    $msg = "";
+    $dire = "king-county-park";
+    $msg .= OneLightbox($dire);
+    print $msg . date("Y-m-d H:i:s");
+
+    function OneLightbox($dire, $photographer = "", $photoDate = "")
+    {
+        $template =  file_get_contents("make2Template.html");
+        $template = str_replace("<title>", "<title>$dire", $template);
+        $template = str_replace("<h1>", "<h1>$dire", $template);
+        $images = "\n";
+        foreach (new DirectoryIterator($dire) as $fileInfo) {
+            if ($fileInfo->isDot()) continue;
+            if ($fileInfo->isDir()) continue;
+            $fileName = $fileInfo->getFilename();
+
+            $images .= "{
+                src:
+                    '$dire/$fileName?auto=format&fit=crop&w=1400&q=80',
+                responsive:
+                    '$dire/$fileName?auto=format&fit=crop&w=480&q=80 480, $dire/$fileName?auto=format&fit=crop&w=800&q=80 800',
+                thumb:
+                    '$dire/thumbs/thumbs_$fileName?auto=format&fit=crop&w=240&q=80',
+                subHtml: 
+                    `<div class=\"lightGallery-captions\">";
+            if ($photographer)
+                $images .= "<h4>Photo by <a href=\"$photographer\">$photographer</a></h4>";
+            if ($photoDate)
+                $images .= "<p>Published on $photoDate</p>";
+
+            $images .= "</div>`
+            },
+        ";
+        }
+        $template = str_replace('dynamicEl: [', 'dynamicEl: ['  . $images, $template);
+
+        file_put_contents("$dire.html", $template);
+        return "Created <a href='//127.0.0.1/lightbox/$dire.html' target='make'> $dire.html</a><br>";
+    }
+
+
+
+    ?>
+
+</body>
+
+</html>
